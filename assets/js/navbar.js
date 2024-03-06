10bda9ac5844c8c39b1fbe896f6d392163d33a96
Byron Coc <cocpalomobyronalejandro@gmail.com>
2024-03-05 19:02:12 -0600

    Correcciones generales
    Se actualizo la navbar a la version mejorada del organigrama
    
    Se combinaron los archivos de js para que funcione la navbar
    
    Se agrego css para hamburguer
    
    Se idento mejor el html y se crearon comentarios para el npm run build

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,47 @@
+// hacer que la pagina siempre cargue desde arriba
+window.onload = function() {
+    window.scrollTo(0, 0);
+  };
+
+//ABRIR Y CERRAR NAVBAR
 let btnToggleNavbar = document.getElementById('small_png2');
 let navbarAnimation =  document.getElementById('navbar');
 
 function toggleNavbar() {   
     navbarAnimation.classList.toggle('open');
 }
-
 btnToggleNavbar.addEventListener('click',toggleNavbar);
 
+// Anclar navbar al cielo de la pagina y animacion de hamburguesa
+// para que la hamburguesa cambie al mismo tiempo que se cierra la navbar
+
+var hamburger = document.querySelector(".hamburger");
+(function () {
+    let navbar = document.querySelector('#navbar');
+    let main = document.querySelector('main');
+    let breakpoint = main.offsetTop - navbar.getBoundingClientRect().height;
+    let isFixed = false;
+
+    function onScroll() {
+        let windowPos = window.scrollY;
+        let navbarHeight = navbar.getBoundingClientRect().height;
+
+        navbar.classList.remove('open');
+        hamburger.classList.remove("is-active");
+
+        if (windowPos >= breakpoint && !isFixed) {
+            navbar.classList.add('navbar_fixed');
+            main.style.marginTop = navbarHeight + 'px';
+            isFixed = true;
+        } else if (windowPos <= breakpoint && isFixed) {
+            navbar.classList.remove('navbar_fixed');
+            main.style.cssText = "margin-top:" + 0;
+            isFixed = false;
+        }
+    }
+
+    document.addEventListener('scroll', () => {
+        requestAnimationFrame(onScroll);
+    });
+    
+})();
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import './assets/js/fixed';
-import './assets/js/navbar';
+import './assets/js/navbar.js';
 import "./node_modules/eins-modal/dist/js/eins-modal.min.js";
 import "./assets/css/index.css";
